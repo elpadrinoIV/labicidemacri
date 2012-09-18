8a51825f18a04e23e373e9ea1064c224ec3bb017
Nico <n@d.com>
2012-09-18 09:42:08 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labicidemacri::Application.config.secret_token = 'eb0359e98cc88c00c1de312c7d7f46b88022d02e01506f14693ef06f73f03fd6396ea9f82f45c88270665b30ba894e29c5de644e43a7fc976a1b8358262689de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Labicidemacri::Application.config.session_store :cookie_store, key: '_labicidemacri_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Labicidemacri::Application.config.session_store :active_record_store
